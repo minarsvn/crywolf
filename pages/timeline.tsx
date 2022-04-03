3a6e128f38af530d2ffc3c6300709732230369af
minarsvn <minarshovon909@gmail.com>
2022-04-03 18:48:27 +0600

    first-commit

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -1,17 +1,21 @@
-import Link from 'next/link';
+import { useMDXComponent } from 'next-contentlayer/hooks';
+import components from 'components/MDXComponents';
+import UsesLayout from 'layouts/uses';
+import { allOtherPages } from 'contentlayer/generated';
+import type { OtherPage } from 'contentlayer/generated';
 
-import Timelines from '../components/Timeline';
-import Container from 'components/Container';
+export default function Uses({ body: { code } }: OtherPage) {
+  const Component = useMDXComponent(code);
 
-export default function Timeline() {
   return (
-    <Container title="Timeline">
-      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-vio dark:text-vio">
-        Timeline
-        </h1>
-      <Timelines />
-      </div>
-    </Container>
+    <UsesLayout>
+      <Component components={components as any} />
+    </UsesLayout>
   );
+}
+
+export async function getStaticProps() {
+  const uses = allOtherPages.find((page) => page.slug === 'uses')!;
+
+  return { props: uses };
 }
