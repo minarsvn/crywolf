ff39125e9ae2511ce97a556488d101888e64bf54
minarsvn <minarshovon909@gmail.com>
2022-04-03 11:23:13 +0600

    first-commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import 'styles/global.css';
-
+import NextNProgress from "nextjs-progressbar";
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <ThemeProvider attribute="class">
+      <NextNProgress
+  color="#05f559"
+  startPosition={0.3}
+  stopDelayMs={200}
+  height={3}
+  showOnShallow={true}
+/>
         <Component {...pageProps} />
       </ThemeProvider>
   );
