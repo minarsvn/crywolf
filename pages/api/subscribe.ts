8bb7ed2ad61048b1396784743614cc0df42e972f
minarsvn <minarshovon909@gmail.com>
2022-04-03 10:41:39 +0600

    first-commit

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,28 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import mailchimp from '@mailchimp/mailchimp_marketing';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_API_SERVER // e.g. us1
+});
+
+export default async (req, res) => {
   const { email } = req.body;
 
   if (!email) {
     return res.status(400).json({ error: 'Email is required' });
   }
 
-  const result = await fetch('https://www.getrevue.co/api/v2/subscribers', {
-    method: 'POST',
-    headers: {
-      Authorization: `Token ${process.env.REVUE_API_KEY}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ email })
-  });
-  const data = await result.json();
+  try {
+    await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
+      email_address: email,
+      status: 'subscribed'
+    });
 
-  if (!result.ok) {
-    return res.status(500).json({ error: data.error.email[0] });
+    return res.status(201).json({ error: '' });
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() });
   }
-
-  return res.status(201).json({ error: '' });
-}
+};
