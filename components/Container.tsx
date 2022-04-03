0f5ea1b6d8d30c0819cd2556c4e91af56d07e184
minarsvn <minarshovon909@gmail.com>
2022-04-03 19:59:31 +0600

    first-commit

--- components/Container.tsx
+++ components/Container.tsx
@@ -38,8 +38,8 @@ export default function Container(props) {
   const { children, ...customMeta } = props;
   const router = useRouter();
   const meta = {
-    title: 'Lee Robinson – Developer, writer, creator.',
-    description: `Front-end developer, JavaScript enthusiast, and course creator.`,
+    title: 'Cry Wolf ',
+    description: `Sleepy Sleepy`,
     image: 'https://leerob.io/static/images/banner.png',
     type: 'website',
     ...customMeta
