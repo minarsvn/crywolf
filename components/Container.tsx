39e80f12f4ba11e8157020de3a13e60da3bdb28e
minarsvn <minarshovon909@gmail.com>
2022-04-03 10:54:44 +0600

    first-commit

--- components/Container.tsx
+++ components/Container.tsx
@@ -75,10 +75,9 @@ export default function Container(props) {
           <div className="ml-[-0.60rem]">
             <MobileMenu />
             <NavItem href="/" text="Home" />
-            <NavItem href="/guestbook" text="Guestbook" />
-            <NavItem href="/dashboard" text="Dashboard" />
             <NavItem href="/blog" text="Blog" />
             <NavItem href="/snippets" text="Snippets" />
+            <NavItem href="/about" text="About" />
           </div>
           <button
             aria-label="Toggle Dark Mode"
