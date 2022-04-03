095753494a2a2f5d2a17441e30e03d7e7cb56124
minarsvn <minarshovon909@gmail.com>
2022-04-03 09:32:57 +0600

    first-commit

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -5,7 +5,6 @@ import ProsCard from 'components/ProsCard';
 import ConsCard from 'components/ConsCard';
 import Gumroad from 'components/metrics/Gumroad';
 import Unsplash from 'components/metrics/Unsplash';
-import Analytics from 'components/metrics/Analytics';
 import YouTube from 'components/metrics/Youtube';
 import Step from 'components/Step';
 import ImageWithTheme from 'components/ImageWithTheme';
@@ -43,7 +42,6 @@ const MDXComponents = {
   ImageWithTheme,
   a: CustomLink,
   Callout,
-  Analytics,
   ConsCard,
   Gumroad,
   ProsCard,
