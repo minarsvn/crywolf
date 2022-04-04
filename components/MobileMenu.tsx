b1346394d09d41ac74f2df1cea58b0749f5b6515
minarsvn <minarshovon909@gmail.com>
2022-04-04 16:28:49 +0600

    first-commit

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -85,14 +85,6 @@ export default function MobileMenu() {
               <a className="flex w-auto pb-4">About</a>
             </Link>
           </li>
-          <li
-            className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
-            style={{ transitionDelay: '350ms' }}
-          >
-            <Link href="/uses">
-              <a className="flex w-auto pb-4">Uses</a>
-            </Link>
-          </li>
         </ul>
       )}
       </div>
